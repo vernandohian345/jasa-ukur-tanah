1b2fd38fe463d58bc046240aefb649ea209aa499
vernandohian345 <vernando767@gmail.com>
2026-01-10 21:36:06 +0700

    update db

--- api/db.js
+++ api/db.js
@@ -1,17 +1,7 @@
-const mysql = require('mysql2/promise');
+import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  connectTimeout: 20000,
+export const db = mysql.createPool({
+  uri: process.env.DATABASE_URL,
   waitForConnections: true,
-  connectionLimit: 3,
+  connectionLimit: 5
 });
-
-module.exports = pool;
