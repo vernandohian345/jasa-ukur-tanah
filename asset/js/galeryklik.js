de1482bdf10b1f068e9ef4258e65c656c1aa684a
DevankaPutra <devankaputra17@gmail.com>
2026-01-17 20:22:11 +0700

    Refactor code structure for improved readability and maintainability

--- asset/js/galeryklik.js
+++ asset/js/galeryklik.js
@@ -15,3 +15,26 @@ function openGallery(images) {
 function closeGallery() {
     document.getElementById('galleryModal').classList.remove('active');
 }
+
+  const zoomModal = document.getElementById("zoomModal");
+  const zoomImage = document.getElementById("zoomImage");
+  const zoomClose = document.getElementById("zoomClose");
+
+  // SEMUA GAMBAR DALAM MODAL GALERI
+  document.addEventListener("click", function(e) {
+    if (e.target.closest("#galleryContent img")) {
+      zoomModal.classList.add("active");
+      zoomImage.src = e.target.src;
+    }
+  });
+
+  // TUTUP ZOOM
+  zoomClose.onclick = () => {
+    zoomModal.classList.remove("active");
+  };
+
+  zoomModal.onclick = (e) => {
+    if (e.target === zoomModal) {
+      zoomModal.classList.remove("active");
+    }
+  };
